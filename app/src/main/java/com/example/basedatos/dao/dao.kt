0dbc8a6c300d8c9d1b7cbcf20c66044f0a6ddd2a
XxTiny2099xX <andreycg2099@gmail.com>
2026-06-03 20:53:33 -0600

    Ejercicio 2

---
+++ app/src/main/java/com/example/basedatos/dao/dao.kt
@@ -0,0 +1,15 @@
+package com.example.basedatos.dao
+
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.basedatos.model.Usuario
+
+interface DAO {
+    interface UsuarioDao {
+        @Insert
+        suspend fun insertarUsuario(usuario: Usuario)
+        @Query("SELECT * FROM usuarios_table")
+        suspend fun obtenerUsuarios():
+                List<Usuario>
+    }
+}
