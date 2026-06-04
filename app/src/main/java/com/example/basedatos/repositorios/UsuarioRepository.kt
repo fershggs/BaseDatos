ecfaa06dfef750906155be1811842bf2f6846232
XxTiny2099xX <andreycg2099@gmail.com>
2026-06-03 21:06:14 -0600

    Ejercicio 5

---
+++ app/src/main/java/com/example/basedatos/repositorios/UsuarioRepository.kt
@@ -0,0 +1,10 @@
+package com.example.basedatos.repositorios
+
+import com.example.basedatos.dao.UsuarioDao
+import com.example.basedatos.model.Usuario
+
+class UsuarioRepository(private val usuarioDao: UsuarioDao) {
+    suspend fun addUsuario(usuario: Usuario) = usuarioDao.insertarUsuario(usuario)
+    suspend fun getAllUsuarios() = usuarioDao.obtenerUsuarios()
+    //suspend fun findUsuario(nombre: String) = usuarioDao.buscarPorNombre(nombre)
+}
